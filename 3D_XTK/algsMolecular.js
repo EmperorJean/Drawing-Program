182eb5978cc6f4d1b786d14154dc9aa9ec028c25
emperorjean <gerard.g.jean@gmail.com>
2023-03-02 23:15:14 -0500

    Created Molecular alg

--- 3D_XTK/algsMolecular.js
+++ 3D_XTK/algsMolecular.js
@@ -1,18 +1,37 @@
 var r;
 var algsMolecular = {}
+var numNodes = 0;
+let lastCoords = null;
+let visitedCoors = [0,0,0]
 
 algsMolecular.drawSphere = function (x, y, z, radius, color) {
     s = new X.sphere();
     s.radius = radius;
     s.color = color;
-    s.center = [x, y, z]
+    s.center = lastCoords ? lastCoords : [x, y, z] 
+    let ran = Math.floor(Math.random() * 100 + 1);
+
+    if(ran > 25)
+    {
+        algsMolecular.drawLine(s.center, [s.center[0]  + Math.floor(Math.random () * algsMolecular.maxLineLength + algsMolecular.minLineLength),s.center[1] + Math.floor(Math.random () * algsMolecular.maxLineLength + algsMolecular.minLineLength),s.center[2]+Math.floor(Math.random () * algsMolecular.maxLineLength + algsMolecular.minLineLength)])
+    }else{
+        lastCoords = null;
+    }
+    
     r.add(s)
     r.render();
 }
 
-algsMolecular.drawLine = function()
+algsMolecular.drawLine = function(start, end)
 {
-
+    cylinder = new X.cylinder();
+    cylinder.start = start;
+    cylinder.end = end;
+    cylinder.radius = algsMolecular.lineThickness;
+    cylinder.color = [1, 1, 1];
+    cylinder.caption = 'cylinder 2';
+    lastCoords = end;
+    r.add(cylinder)
 }
 
 algsMolecular.drawOneStep = function () {
@@ -20,7 +39,11 @@ algsMolecular.drawOneStep = function () {
         clearInterval(algsMolecular.loop);
         return false;
     }
-    
+
+    let x = Math.floor(width * Math.random()) + ++visitedCoors[0]*5;
+    let y = Math.floor(width * Math.random()) + ++visitedCoors[1]*5;
+    let z = Math.floor(width * Math.random()) + ++visitedCoors[2];
+    algsMolecular.drawSphere(x, y, z, algsMolecular.sphereRadiusMax, algsMolecular.sphereColor);
     algsMolecular.numOfSteps++;
 }
 
--- 3D_XTK/algsMolecular_Params.js
+++ 3D_XTK/algsMolecular_Params.js
@@ -1,9 +1,9 @@
 /* Parameters */
 algsMolecular.speed = 0;
-algsMolecular.maxNumOfSteps = 50;
-algsMolecular.sphereRadiusMax = 15;
+algsMolecular.maxNumOfSteps = 100;
+algsMolecular.sphereRadiusMax = 20;
 algsMolecular.sphereRadiusMin = 15;
-algsMolecular.maxLineLength = 15;
-algsMolecular.minLineLength = 5;
-algsMolecular.lineThickness = 3;
-algsMolecular.sphereColor = [1, 1, 1];
+algsMolecular.maxLineLength = 50;
+algsMolecular.minLineLength = 25;
+algsMolecular.lineThickness = 5;
+algsMolecular.sphereColor = [1, 0, 0];

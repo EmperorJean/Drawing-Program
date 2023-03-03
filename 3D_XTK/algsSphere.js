1895ffd4cbccfc750196c6bf7122efe5d4274013
Jake-br <jakebreen21@gmail.com>
2023-03-02 21:30:02 -0500

    Revert "Switched from nodeJS version of ThreeJS to web version"
    This reverts commit 254e7b9cce6f554bdb8aee621df90c6103a7f7c1.

---
+++ 3D_XTK/algsSphere.js
@@ -0,0 +1,44 @@
+var r;
+var algSphere = {}
+
+algSphere.drawSphere = function (x, y, z, radius, color) {
+    s = new X.sphere();
+    s.radius = radius;
+    s.color = color;
+    s.center = [x, y, z]
+    r.add(s)
+    r.render();
+}
+
+algSphere.drawOneStep = function () {
+    if (algSphere.numOfSteps >= algSphere.maxNumOfSteps) {
+        clearInterval(algSphere.loop);
+        return false;
+    }
+    let x = Math.floor(width * Math.random());
+    let y = Math.floor(width * Math.random());
+    let z = Math.floor(width * Math.random());
+    algSphere.drawSphere(x, y, z, algSphere.sphereRadius, algSphere.sphereColor);
+    algSphere.numOfSteps++;
+}
+
+
+algSphere.reset = function () {
+    // Check if already running
+    algSphere.pause();
+    // Initialize values
+    algSphere.numOfSteps = 0;
+}
+
+algSphere.initialize = function () {
+    algSphere.reset();
+}
+
+algSphere.pause = function () {
+    if ("loop" in algSphere) {
+        clearInterval(algSphere.loop);
+    }
+}
+algSphere.start = function () {
+    algSphere.loop = setInterval(algSphere.drawOneStep, algSphere.speed);
+}
---
+++ 3D_XTK/algsSphere_Params.js
@@ -0,0 +1,5 @@
+/* Parameters */
+algSphere.speed = 0;
+algSphere.maxNumOfSteps = 50;
+algSphere.sphereRadius = 15;
+algSphere.sphereColor = [1, 1, 1];
